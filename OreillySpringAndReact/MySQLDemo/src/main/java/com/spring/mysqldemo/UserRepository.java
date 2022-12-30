d9708052dcc4ce7d5ad002b2d0384dd7ae7571c5
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2022-12-30 10:35:29 +0000

    updated

--- OreillySpringAndReact/MySQLDemo/src/main/java/com/spring/mysqldemo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spring.mysqldemo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.spring.mysqldemo.User;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-}
