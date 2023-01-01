ae6248c5ea592f6865d5912dfcd918c6700567de
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2023-01-01 16:47:49 +0000

    frontend updated

--- OreillyFullStack/CarDataBaseWebServiceFrontend/src/main/java/com/packt/cardatabasewebservicefrontend/domain/CarRepository.java
+++ OreillyFullStack/CarDataBaseWebServiceFrontend/src/main/java/com/packt/cardatabasewebservicefrontend/domain/CarRepository.java
@@ -2,9 +2,11 @@ package com.packt.cardatabasewebservicefrontend.domain;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
 
     List<Car> findByBrand(@Param("brand") String brand);
