57eab7fe424b8e9dfddbebe1ebc77b6180e5a6c1
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2023-03-16 19:53:32 +0000

    routing added to react-udemy

--- react-udemy/dummy-backend/app.js
+++ react-udemy/dummy-backend/app.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 
 app.get('/posts', async (reg, res) => {
     const storedPosts = await getStoredPosts();
-    //await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
+    await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
     res.json({ posts: storePosts});
 });
 
--- react-udemy/starting-project/src/index.js
+++ react-udemy/starting-project/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import NewPost from './components/NewPost';
+import RootLayout from './routes/RootLayout';
 
+
+const router = createBrowserRouter([
+    { path: '/', element: <RootLayout />, children: [
+        { path: '/', element: <App /> },
+        { path: '/create', element: <NewPost /> },
+    ],
+ },
+  
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-            <App/>
+        <RouterProvider router={router} />
     </React.StrictMode>
 );
 
