9d4339098218da3867c4a8a557ecb882df191c3e
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2023-04-10 19:39:54 +0100

    updated

---
+++ springmysql/src/main/java/com/example/springmysql/controller/PersonController.java
@@ -0,0 +1,45 @@
+package com.example.springmysql.controller;
+
+import com.example.springmysql.model.Person;
+import com.example.springmysql.service.PersonService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/api/persons")
+public class PersonController {
+
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @GetMapping
+    public List<Person> getAllPersons() {
+        return personService.getAllPersons();
+    }
+
+    @GetMapping("/{id}")
+    public Person getPersonById(@PathVariable Long id) {
+        return personService.getPersonById(id);
+    }
+
+    @PostMapping
+    public Person createPerson(@RequestBody Person person) {
+        return personService.createPerson(person);
+    }
+
+    @PutMapping("/{id}")
+    public Person updatePerson(@PathVariable Long id, @RequestBody Person person) {
+        return personService.updatePerson(id, person);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePerson(@PathVariable Long id) {
+        personService.deletePerson(id);
+    }
+
+}
