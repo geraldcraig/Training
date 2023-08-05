7523012a73b5ff78eb88559b53fbf483213230b7
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2023-08-05 19:27:33 +0100

    websocket app added

--- JavaScriptEssentials1/main.js
+++ JavaScriptEssentials1/main.js
@@ -1,1 +1,14 @@
-console.log("Hello, World!");
+function outer() {
+    let name = "outer";
+    let str = inner();
+    return str;
+}
+
+function inner() {
+    let name = "inner";
+    return "Hello !";
+}
+
+console.log("before outer() call");
+console.log(outer());
+console.log("after outer() call");
---
+++ kafka-chat-server/src/test/java/com/example/kafkachatserver/KafkaChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kafkachatserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaChatServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
