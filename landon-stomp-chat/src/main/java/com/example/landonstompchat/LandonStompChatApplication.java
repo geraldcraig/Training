7e62395850a9b2ddbce10e17b24138fa29227fdf
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2023-08-13 21:48:07 +0100

    landon-stomp-chat added

---
+++ landon-stomp-chat/src/main/java/com/example/landonstompchat/LandonStompChatApplication.java
@@ -0,0 +1,12 @@
+package com.example.landonstompchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandonStompChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandonStompChatApplication.class, args);
+    }
+}
---
+++ landon-stomp-chat/src/test/java/com/example/landonstompchat/LandonStompChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.landonstompchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandonStompChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
