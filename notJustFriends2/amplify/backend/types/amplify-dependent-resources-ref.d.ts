f325cf3905fb1c76c23121342903486274480d65
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2023-11-02 22:24:28 +0000

    updated

---
+++ notJustFriends2/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "notjustfriends": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "notjustfriends": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3notjustfriendsstorage6ebbad18": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
