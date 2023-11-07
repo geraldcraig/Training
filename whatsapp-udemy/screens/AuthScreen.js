72bead1206e200954c8185928423377bbbb0a6b1
geraldcraig <55385455+geraldcraig@users.noreply.github.com>
2023-11-07 21:41:43 +0000

    updated

---
+++ whatsapp-udemy/screens/AuthScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import PageContainer from "../components/PageContainer";
+
+const AuthScreen = (props) => {
+  return <SafeAreaView style={{ flex: 1}}>
+      <PageContainer>
+        <Text>Hello</Text>
+      </PageContainer>
+    </SafeAreaView>
+};
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default AuthScreen;
