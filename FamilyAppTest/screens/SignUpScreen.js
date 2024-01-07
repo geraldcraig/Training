49f724187dcec3f1375a374194e3244862d7047a
Gerald Craig <geraldcraig@me.com>
2024-01-07 19:42:35 +0000

    family app auth completed

--- FamilyAppTest/screens/SignUpScreen.js
+++ FamilyAppTest/screens/SignUpScreen.js
@@ -18,8 +18,8 @@ function SignupScreen() {
             authCtx.authenticate(token);
         } catch (error) {
             Alert.alert('Authentication failed', 'Could not create user, please check your input and try again later!');
+            setIsAuthenticating(false);
         }
-        setIsAuthenticating(false);
     }
 
     if (isAuthenticating) {
