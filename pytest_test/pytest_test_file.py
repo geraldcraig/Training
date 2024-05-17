06c7d90a0e2a30b8ae55a6a70bfc566d4848e3a0
geraldcraig <geraldcraig@me.com>
2024-05-17 21:43:19 +0100

    updated

--- pytest_test/pytest_test_file.py
+++
@@ -1,9 +0,0 @@
-
-def test_Me():
-    assert True
-
-def test_Me2():
-    assert True
-
-def not_a_test():
-    assert True
--- react-native-freecodecamp/src/screens/index.js
+++
@@ -1,3 +0,0 @@
-export { default as LoginScreen } from './LoginScreen/LoginScreen';
-export { default as HomeScreen } from './HomeScreen/HomeScreen';
-export { default as RegistrationScreeen } from './RegistrationScreen/RegistrationScreen';
